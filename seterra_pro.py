27d2044518174308e7aff95bf1915fee1d3f6aa4
frumsrekovni <joshua.backstrom@gmail.com>
2022-04-17 00:53:16 +0200

    Now boots web driver with an extension, which changed some of the logic. Installs latest chrome driver at start-up. Solves several maps in a row

--- seterra_pro.py
+++ seterra_pro.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -18,66 +21,54 @@ from os.path import isfile, join
 
 
 SOLUTION = "WEBELEMENT"
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-SITE = "https://online.seterra.com/en/vgp/3163"
 prev_country = " "
+ser = Service(ChromeDriverManager().install())
+MAPS = ["https://online.seterra.com/en/vgp/3007","https://online.seterra.com/en/vgp/3003","https://online.seterra.com/en/vgp/3163","https://online.seterra.com/en/vgp/3254","https://online.seterra.com/en/vgp/3123","https://online.seterra.com/en/vgp/3023"]
+chrome_options = Options()
+chrome_options.add_extension(r'C:\Users\Joshua\Python\pro_seterra_player\extension_1_42_4_0.crx') # Noticed that there are different pop-ups that destroy the script in different ways. So now chrome boots up with ad block.
+driver = webdriver.Chrome(service=ser,options=chrome_options)
+for x in MAPS:
+    driver.get(x)
+    driver.maximize_window()
+    driver.execute_script("window.scrollTo(0, 600)")  # Scroll down
+    #This zooms in the web browser whatever amount you enter after zoom=. Meant to solve the issue of the clickable surfaces not being visible but is now instead solved with this driver.execute_script("window.scrollTo(0, 500)") 
 
-driver = webdriver.Chrome(PATH)
-driver.get(SITE)
-driver.maximize_window()
-driver.execute_script("window.scrollTo(0, 600)")  # Scroll down
-#This zooms in the web browser whatever amount you enter after zoom=. Meant to solve the issue of the clickable surfaces not being visible but is now instead solved with this driver.execute_script("window.scrollTo(0, 500)") 
-#driver.execute_script("document.body.style.zoom='50%'")
+    #WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "accept-choices"))) # Waits until the terms and conditions window is visible before clicking it
+    #driver.find_element(By.ID, "accept-choices").click() # Accept the terms and conditions so it goes away
+    #driver.execute_script("document.body.style.zoom='50%'")
+    driver.find_element(By.ID, "cmdRestart").click() # Press the restart button in order to get a hilariously fast solve time
 
-WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "accept-choices"))) # Waits until the terms and conditions window is visible before clicking it
-driver.find_element(By.ID, "accept-choices").click() # Accept the terms and conditions so it goes away
-driver.find_element(By.ID, "cmdRestart").click() # Press the restart button in order to get a hilariously fast solve time
+    #Tried getting rid of the water that kept intercepting clicks but the problem is that 
+    #driver.execute_script("arguments[0].setAttribute('width',arguments[1])",driver.find_element(By.ID, "WATER"), "0")
+    if(SOLUTION == "WEBELEMENT"):
+        while(True):
+            currentQuestion = str(driver.find_element(By.ID, "currQuestion").text).replace('| Click on ','') # Convert to string and remove the filler text
+            if(currentQuestion == ""):
+                break
+            print("Current Question: ",currentQuestion)
 
-
-#Tried getting rid of the water that kept intercepting clicks but the problem is that 
-#driver.execute_script("arguments[0].setAttribute('width',arguments[1])",driver.find_element(By.ID, "WATER"), "0")
-if(SOLUTION == "WEBELEMENT"):
-    while(True):
-        currentQuestion = str(driver.find_element(By.ID, "currQuestion").text).replace('| Click on ','') # Convert to string and remove the filler text
-        print("Current Question: ",currentQuestion)
-
-        #This works to get the tiny rect over each country
-        #countryInfo = driver.find_element(By.XPATH, "//path[contains(.,'GERMANY')]/following-sibling::rect")
-        #Another option to achieve to same thing
-        #countryInfo = driver.find_element(By.ID, "NETHERLANDS").find_element(By.XPATH, "//*[name()='rect']").get_attribute("x")
-
-        #The info in data-qText is the most reliable in relation to what is parsed from currentQuestion
-        #move_to_element(driver.find_element(By.XPATH,"//*[@data-qText='"+currentQuestion+"']"))
-        
-        #WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//*[@data-qText='"+currentQuestion+"']")))
-        #driver.find_element(By.XPATH,"//*[@data-qText='"+currentQuestion+"']").click()
-        #JavascriptExecutor executor = 
-        #element = driver.find_element(By.XPATH,"//*[@data-qText='"+currentQuestion+"']")
-        #driver.execute_script("arguments[0].click();", element)
-
-        try:
-            driver.find_element(By.XPATH,"//*[@data-qText='"+currentQuestion+"']").click()      
-        except NoSuchElementException:
-            print("IM IN NO SUCH ELEMENT EXCEPTION")
-            element = driver.find_element(By.XPATH,"//*[@data-qText='"+currentQuestion+"']").find_element(By.TAG_NAME,"rect")
-            webdriver.ActionChains(driver).move_to_element(element).click(element ).perform()
-        except ElementClickInterceptedException:
-            print("IM IN CLICK INTERCEPTED EXCEPTION")
-            if(currentQuestion == prev_country):
-                element = driver.find_element(By.XPATH,"//*[@data-qText='"+currentQuestion+"']").find_element(By.TAG_NAME,"path")
-                webdriver.ActionChains(driver).move_to_element(element).click(element ).perform()
-            else:
+            try:
+                driver.find_element(By.XPATH,"//*[@data-qText='"+currentQuestion+"']").click()      
+            except NoSuchElementException:
+                print("IM IN NO SUCH ELEMENT EXCEPTION")
                 element = driver.find_element(By.XPATH,"//*[@data-qText='"+currentQuestion+"']").find_element(By.TAG_NAME,"rect")
                 webdriver.ActionChains(driver).move_to_element(element).click(element ).perform()
-        #pyautogui.moveTo(872, 148*2,1)
-        driver.execute_script("window.scrollTo(0, 800)")
-        #driver.execute_script("arguments[0].click()", element)
-        prev_country = currentQuestion
-elif(SOLUTION == "TEMPLATE"):
-    swe_img = cv.imread('country_captures/sweden.png', cv.IMREAD_UNCHANGED)
+            except ElementClickInterceptedException:
+                print("IM IN CLICK INTERCEPTED EXCEPTION")
+                if(currentQuestion == prev_country):
+                    element = driver.find_element(By.XPATH,"//*[@data-qText='"+currentQuestion+"']").find_element(By.TAG_NAME,"path")
+                    webdriver.ActionChains(driver).move_to_element(element).click(element ).perform()
+                else:
+                    element = driver.find_element(By.XPATH,"//*[@data-qText='"+currentQuestion+"']").find_element(By.TAG_NAME,"rect")
+                    webdriver.ActionChains(driver).move_to_element(element).click(element ).perform()
 
+            driver.execute_script("window.scrollTo(0, 750)")
+            prev_country = currentQuestion
+    elif(SOLUTION == "TEMPLATE"):
+        swe_img = cv.imread('country_captures/sweden.png', cv.IMREAD_UNCHANGED)
 
-time.sleep(100)
+    print("MAP SOLVED!")
+    time.sleep(2)
 driver.quit()
 
 
